1cb957f7195d2c51c7744c975e38da0e739c7fdc
Sharjil Bin Yousaf <sharjil@developers.studio>
2023-06-11 17:39:56 +0500

    task 4 completed

--- database/migrations/2022_06_13_075211_create_cinema_schema.php
+++ database/migrations/2022_06_13_075211_create_cinema_schema.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateCinemaSchema extends Migration
 {
-    /** ToDo: Create a migration that creates all tables for the following user stories
+    /** ToDo: Create a laravel migration that creates all tables for the following user stories
 
     For an example on how a UI for an api using this might look like, please try to book a show at https://in.bookmyshow.com/.
     To not introduce additional complexity, please consider only one cinema.
@@ -36,7 +36,75 @@ class CreateCinemaSchema extends Migration
      */
     public function up()
     {
-        throw new \Exception('implement in coding task 4, you can ignore this exception if you are just running the initial migrations.');
+        /**
+         * Create movies table
+         */
+        if (!Schema::hasTable('movies')) {
+            Schema::create('movies', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('title');
+                $table->text('description');
+                $table->time('duration');
+                $table->timestamps();
+            });
+        }
+
+        /**
+         * Create cinemas table
+         */
+        if (!Schema::hasTable('cinemas')) {
+            Schema::create('cinemas', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('name');
+                $table->text('address');
+                $table->timestamps();
+            });
+        }
+
+        /**
+         * Create shows table
+         */
+        if (!Schema::hasTable('shows')) {
+            Schema::create('shows', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('movie_id')->unsigned();
+                $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+                $table->integer('cinema_id')->unsigned();
+                $table->foreign('cinema_id')->references('id')->on('cinemas')->onDelete('cascade');
+                $table->time('show_time');
+                $table->integer('booked_seats')->default('0');
+                $table->timestamps();
+            });
+        }
+
+         /**
+         * Create pricing table
+         */
+        if (!Schema::hasTable('pricing')) {
+            Schema::create('pricing', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('show_id')->unsigned();
+                $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+                $table->double('price', 8, 2);
+                $table->timestamps();
+            });
+        }
+
+        /**
+         * Create seats table
+         */
+        if (!Schema::hasTable('seats')) {
+            Schema::create('seats', function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->integer('show_id')->unsigned();
+                $table->foreign('show_id')->references('id')->on('shows')->onDelete('cascade');
+                $table->integer('seat_number')->unsigned();
+                $table->enum('seat_type', ['VIP', 'COUPLE', 'SUPER_VIP']);
+                $table->boolean('is_booked')->default(0);
+                $table->timestamps();
+            });
+        }
+
     }
 
     /**
@@ -46,5 +114,11 @@ class CreateCinemaSchema extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('movies');
+        Schema::dropIfExists('cinemas');
+        Schema::dropIfExists('shows');
+        Schema::dropIfExists('pricing');
+        Schema::dropIfExists('seats');
+
     }
 }
